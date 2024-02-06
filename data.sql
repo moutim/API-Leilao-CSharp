ce05270f32728234acb118c7cb42d5a87953e933
Vitor Moutim <moutimg@gmail.com>
2024-02-05 23:15:27 -0300

    feat: create solution for web api and tests

---
+++ data.sql
@@ -0,0 +1,151 @@
+-- MySQL dump 10.16  Distrib 10.1.48-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: db
+-- ------------------------------------------------------
+-- Server version	10.1.48-MariaDB-0+deb9u2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Auctions`
+--
+
+DROP TABLE IF EXISTS `Auctions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Auctions` (
+  `Id` tinyint(4) DEFAULT NULL,
+  `Name` varchar(20) DEFAULT NULL,
+  `Starts` varchar(23) DEFAULT NULL,
+  `Ends` varchar(23) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Auctions`
+--
+
+LOCK TABLES `Auctions` WRITE;
+/*!40000 ALTER TABLE `Auctions` DISABLE KEYS */;
+INSERT INTO `Auctions` VALUES (1,'Leilao da Rocketseat','2024-01-20 00:00:00.000','2024-01-29 23:59:59.000');
+/*!40000 ALTER TABLE `Auctions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Items`
+--
+
+DROP TABLE IF EXISTS `Items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Items` (
+  `Id` tinyint(4) DEFAULT NULL,
+  `Name` varchar(28) DEFAULT NULL,
+  `Brand` varchar(8) DEFAULT NULL,
+  `Condition` tinyint(4) DEFAULT NULL,
+  `BasePrice` decimal(5,1) DEFAULT NULL,
+  `AuctionId` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Items`
+--
+
+LOCK TABLES `Items` WRITE;
+/*!40000 ALTER TABLE `Items` DISABLE KEYS */;
+INSERT INTO `Items` VALUES (1,'Galaxy S21','Samsung',0,800.0,1),(2,'Macbook Pro','Apple',1,1000.0,1),(3,'Camera Canon EOS','Canon',2,1200.0,1),(4,'Headphone WH-1000XM5','Sony',1,200.0,1),(5,'Nintendo Switch OLED','Nintendo',0,80.0,1),(6,'Drone com Camera Mavic Air 2','DJI',2,700.0,1),(7,'Kindle Paperwhite 2022','Amazon',0,50.0,1);
+/*!40000 ALTER TABLE `Items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Offers`
+--
+
+DROP TABLE IF EXISTS `Offers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Offers` (
+  `Id` varchar(0) DEFAULT NULL,
+  `CreatedOn` varchar(0) DEFAULT NULL,
+  `Price` varchar(0) DEFAULT NULL,
+  `ItemId` varchar(0) DEFAULT NULL,
+  `UserId` varchar(0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Offers`
+--
+
+LOCK TABLES `Offers` WRITE;
+/*!40000 ALTER TABLE `Offers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Offers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `Id` tinyint(4) DEFAULT NULL,
+  `Name` varchar(9) DEFAULT NULL,
+  `Email` varchar(23) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'Cristiano','cristiano@cristiano.com'),(2,'Tatiana','tatiana@tatiana.com'),(3,'Lucimar','lucimar@lucimar.com');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sqlite_sequence`
+--
+
+DROP TABLE IF EXISTS `sqlite_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sqlite_sequence` (
+  `name` varchar(8) DEFAULT NULL,
+  `seq` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sqlite_sequence`
+--
+
+LOCK TABLES `sqlite_sequence` WRITE;
+/*!40000 ALTER TABLE `sqlite_sequence` DISABLE KEYS */;
+INSERT INTO `sqlite_sequence` VALUES ('Items',7),('Users',3),('Offers',0),('Auctions',1);
+/*!40000 ALTER TABLE `sqlite_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-09-18 10:46:27
